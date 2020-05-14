a2b71fb2a3d7996539c08f13d9f530077556e540
Varsha <hp@DESKTOP-4IENAI5>
2020-05-15 00:59:41 +0530

    FSJ0420A113

--- src/utility/CheckBlogPost.java
+++ src/utility/CheckBlogPost.java
@@ -1,1 +1,33 @@
 package utility;
+
+import model.Blog;
+
+public class CheckBlogPost extends OffensiveWordsChecker implements OffensiveWordsInterface{
+	String[] offensiveWords= {"babeland","apeshit","ar5e","areola", "a55hole","acrotomophilia","aeolus","ahole","alabama hot pocket","	alaskan pipeline","anal","anal impaler","anal leakage","analprobe","anilingus"," aryan","ass", "ass fuck", " ass fuck"," ass hole","areole","arian", "arrse", "arse"," arsehole"};
+	
+	@Override
+	public boolean checkBlogTitle(Blog blog) {
+		for(int i=0;i<offensiveWords.length;i++) {
+			if(blog.getBlogTitle()==offensiveWords[i])
+				return false;
+		}
+		return true;
+	}
+
+	@Override
+	public boolean checkBlogDescription(Blog blog) {
+		for(String offensivewords : offensiveWords) {
+			if (blog.getBlogDescription().contains(offensivewords))
+	            return false;
+		}
+		return true;
+	}
+
+	@Override
+	public boolean checkBlog(Blog blog) {
+		if(checkBlogTitle(blog) && checkBlogDescription(blog))
+			return true;
+		return false;
+	}
+	
+}
